49f1885cc03ae303ab821e2aefe0d6b2e7d0644d
Stefan Schenk <stefan_schenk@hotmail.com>
2024-10-22 20:18:23 +0200

    Use uvloop.

--- slipstream/__init__.py
+++ slipstream/__init__.py
@@ -1,8 +1,12 @@
 """Top level objects."""
 
+import uvloop
+
 from slipstream.__version__ import VERSION
 from slipstream.caching import Cache
 from slipstream.core import Conf, Topic, handle, stream
+
+uvloop.install()
 
 __all__ = (
     'VERSION',
