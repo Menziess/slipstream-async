380afe84e8cd377d526d6a4d726102f75e46fe79
Stefan Schenk <stefan_schenk@hotmail.com>
2024-10-28 22:25:43 +0100

    Remove uvloop (leave it up to user).

--- slipstream/__init__.py
+++ slipstream/__init__.py
@@ -1,18 +1,14 @@
 """Top level objects."""
 
-import uvloop
-
 from slipstream.__version__ import VERSION
 from slipstream.caching import Cache
 from slipstream.core import Conf, Topic, handle, stream
 
-uvloop.install()
-
-__all__ = (
+__all__ = [
     'VERSION',
     'Conf',
     'Topic',
     'Cache',
     'handle',
     'stream',
-)
+]
