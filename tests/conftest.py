c07ce039eaecb60851099831b9536ed6004ad78b
Stefan Schenk <stefan_schenk@hotmail.com>
2025-03-04 15:14:57 +0100

    Some tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,10 +3,22 @@
 import signal
 from asyncio import sleep
 from contextlib import contextmanager
-from typing import AsyncIterable, Iterable, Iterator
+from typing import Any, AsyncIterable, Iterable, Iterator
 
 from pytest import fixture
 from testcontainers.kafka import KafkaContainer
+
+from slipstream.core import Conf
+from slipstream.interfaces import ICache, Key
+from slipstream.utils import Singleton
+
+
+@fixture(autouse=True, scope='module')
+def reset_Conf():
+    """Clean Conf singleton each test."""
+    if Conf in Singleton._instances:
+        del Singleton._instances[Conf]
+
 
 try:
     from slipstream import Cache
@@ -63,3 +75,37 @@ async def emoji():
     """Demo async iterable."""
     for emoji in '🏆📞🐟👌':
         yield emoji
+
+
+class MockCache(ICache):
+    """Replaces persistent RocksDict for testing purposes."""
+
+    def __init__(self):
+        """Set dict as internal store.."""
+        self._store = {}
+
+    async def __call__(self, key: Key, val: Any) -> None:
+        """Call cache to set item."""
+        self._store[key] = val
+
+    def __contains__(self, key: Key) -> bool:
+        """Key exists in db."""
+        return key in self._store
+
+    def __delitem__(self, key: Key) -> None:
+        """Delete item from db."""
+        del self._store[key]
+
+    def __getitem__(self, key: Key | list[Key]) -> Any:
+        """Get item from db or None."""
+        return self._store.get(key, None)
+
+    def __setitem__(self, key: Key, val: Any) -> None:
+        """Set item in db."""
+        self._store[key] = val
+
+
+@fixture
+def mock_cache():
+    """Get cache for testing."""
+    return MockCache()
